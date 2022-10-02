ed1c9e0f345d4ff431741cd5ca019687cce29ab7
pankaj-rgb <58622744+pankaj-rgb@users.noreply.github.com>
2022-10-02 21:40:00 +0530

    Create Generate_Parenthesis.cpp
    added code for the generate parenthesis

---
+++ recursion/Generate_Parenthesis.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<string>ans;
+   
+    
+    void solve(int open, int close,int n, string s)
+    {   
+        
+        if(open==n && close==n)
+        {
+            ans.push_back(s);
+            return;
+        }
+        
+        // if(open<0 || close<0)
+        // {
+        //     return ;
+        // }
+        if(open<n)
+        {
+        solve(open+1, close,n, s+"(");
+        }
+        
+        if(open>close && close<n)
+        {
+        solve(open, close+1,n, s+")" );
+        }
+        
+    }
+    vector<string> generateParenthesis(int n) {
+        ans.clear();
+        string s="";
+        
+        int open=n,close=n;
+        solve(0,0,n , s);
+        return ans;
+        
+    }
+};
